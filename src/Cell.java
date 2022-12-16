b1bb2aba4e25598fa3b472e0101354cb0060ff92
ludovicpeysson9 <ludovic.peysson@le-campus-numerique.fr>
2022-12-16 16:08:42 +0100

    isOver ajoutee, finir isWinning

--- src/Cell.java
+++ src/Cell.java
@@ -1,15 +1,9 @@
 import java.util.Arrays;
 
 public class Cell {
-//    String croix = "| X ";
-//    String rond = "| O ";
-//    String vide = "|   ";
     String representation = "|   ";
 
     public String getRepresentation(){
         return representation;
     }
-//    public void setRepresentation(int abscisse, int ordonnee){ // Donner coordonnées en parametres
-//        representation = ; // Donner la representation du joueur actif
-//    }
 }
--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,3 +1,4 @@
+import java.util.List;
 import java.util.Scanner;
 
 public class TicTacToe {
@@ -5,7 +6,9 @@ public class TicTacToe {
 
     // Définit la taille du plateau
     final int size = 3;
-    Cell[][] plateau = new Cell[size][size]; // Déclaration d'une variable tableau de type Cell a plusieurs dimensions de taille max size (3)
+    final int drawCounter = (size * size); // Gère la modularité du plateau.
+    int gameCounter = 0;
+    Cell[][] plateau = new Cell[size][size]; // Déclaration d'une variable tableau de type Cell a plusieurs dimensions de taille max modulaire size (3)
 
     Player player1 = new Player("| X ", 1);
     Player player2 = new Player("| O ", 2);
@@ -15,16 +18,19 @@ public class TicTacToe {
     }
 
     // Fonction qui affiche le plateau
+    // TODO Rendre l'affichage du tableau modulaire (.repeat() pour multiplier les chaines de caracteres).
     public void display(){
         System.out.print("     0   1   2");
         System.out.println();
-        for(int i = 0; i < 3; i++){
+        for(int i = 0; i < size; i++){
             System.out.print("   ");
+//            System.out.println("        " + i +"   ");
             System.out.print("-------------");
             System.out.println();
             System.out.print(i);
             System.out.print("  ");
-            for(int j = 0; j < 3; j++){
+            for(int j = 0; j < size; j++){
+
 //                System.out.print(plateau[i][j].getRepresentation());
                 //FIXME tableau inversé pour avoir les coordonnées en abscisse et en ordonnées
                 System.out.print(plateau[j][i].getRepresentation());
@@ -64,9 +70,10 @@ public class TicTacToe {
     public int[] getCoordsWithinBounds() {
         int x = -1;
         int y = -1;
+        int tailleMax = this.size -1;
 
-        while (x < 0 || x > 2) {
-            System.out.println("Entrez l'abscisse' de la case libre ciblée sous forme d'entier entre 0 et 2");
+        while (x < 0 || x > tailleMax) {
+            System.out.println("Entrez l'abscisse' de la case libre ciblée sous forme d'entier entre 0 et " + tailleMax);
             if (scanner.hasNextInt()) {
                 x = scanner.nextInt();
             } else {
@@ -74,8 +81,8 @@ public class TicTacToe {
                 scanner.next();
             }
         }
-        while (y < 0 || y > 2) {
-            System.out.println("Entrez l'ordonnée' de la case libre ciblée sous forme d'entier entre 0 et 2");
+        while (y < 0 || y > tailleMax) {
+            System.out.println("Entrez l'ordonnée' de la case libre ciblée sous forme d'entier entre 0 et " + tailleMax );
             if (scanner.hasNextInt()) {
                 y = scanner.nextInt();
             } else {
@@ -85,12 +92,9 @@ public class TicTacToe {
         }
         return new int[]{x, y};
 
-//        System.out.println(x);
-//        System.out.println(y);
 //        int[] coords = new int[]{x, y};
 //        return coords;
     }
-
     public boolean cellIsEmpty(int abscisse, int ordonnee){
 
         if(plateau[abscisse][ordonnee].representation == "|   " ){
@@ -127,48 +131,187 @@ public class TicTacToe {
 
         return;
     }
-    // Fonction qui définit si le jeu est terminé (plateau rempli ou 3 pions alignés)
-    // TODO Implémenter isOver() pour déterminer la fin de partie
 
-    public boolean isOver(){
+    public boolean isOver(int gameCounter, int drawCounter){ // Prend en paramètre une variable qui determinera si le jeu est fini un booleen
+
+        if(gameCounter < drawCounter){
+            this.gameCounter += 1;
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    // TODO
+    public Cell[] getRows(){
+        for(int i = 0; i < size; i++){
+
+        }
+        return null;
+    }
 
 
-//         Vérifie si le plateau est rempli
-//         Test si il y a 3 X ou 3 O aux coordonées horizontales : [0,0 ; 1,0 ; 2,0 || 0,1 ; 1,1 ; 2,1 || 0,2 ; 1,2 ; 2,2] ou
-//         verticales : [0,0 ; 0,1 ; 0,2 || 1,0 ; 1,1 ; 1,2 || 2,0 ; 2,1 ; 2,2] ou
-//         diagonales : [0,0 ; 1,1 , 2,2 || 0,2 ; 1,1 ; 2,0]
-        return true;
+    public boolean isWinningline(Cell[] row){
+        return false;
     }
 
     //TODO A factoriser
-    public void isWinning1(){
-        System.out.println("Le joueur1 a gagné ! ");
+    public boolean isWinning(){
 
-    }
-    public void isWinning2(){
-        System.out.println("Le joueur2 a gagné ! ");
-
-    }
-
-    //FIXME Test de la fonction avec partieFinie = false. Modifier une fois isWinning() et isOver() implémentées
-
-    public void deroulementPartie(){
-//        boolean partieFinie = isOver();
-        boolean partieFinie = false;
-        tourJoueur1();
-        tourJoueur2();
-        while(!partieFinie){
-            tourJoueur1();
-//            partieFinie = isOver();
-            partieFinie = false;
-            if (!partieFinie){
-                tourJoueur2();
-                partieFinie = false;
-            }else{
-                break;
+        for(int i = 0; i < size; i++){
+            for(int j = 0; j < size; j++){
+                if(plateau[i][j].representation != "|   "){
+                    if(plateau[i][j].representation == "| X " || plateau[i][j].representation == "| O "){
+                        if(plateau[i][j].representation == "| X "){
+                            if( i > 0 && i < size -1){
+                                if(plateau[i-1][j].representation == "| X " && plateau[i+1][j].representation == "| X "){ // Test horizontal
+                                    return true;
+                                }else{
+                                    return false;
+                                }
+                            }
+                            if( j > 0 && j < size -1){
+                                if(plateau[i][j-1].representation == "| X " && plateau[i][j+1].representation == "| X "){ // Test Vertical
+                                    return true;
+                                }else{
+                                    return false;
+                                }
+                            }
+                            if( i > 0 && j > 0 && i < size -1 && j < size-1){
+                                if (plateau[i-1][j-1].representation == "| X " && plateau[i+1][j+1].representation == "| X "){ // Test diagonale
+                                    return true;
+                                }else{
+                                    return false;
+                                }
+                            }
+                        }else
+                        if(plateau[i][j].representation == "| O "){
+                            if( i> 0 && i < size -1){
+                                if(plateau[i-1][j].representation == "| O " && plateau[i+1][j].representation == "| O "){ // Test horizontal
+                                    return true;
+                                }else{
+                                    return false;
+                                }
+                            }
+                            if( j > 0 && j < size -1){
+                                if(plateau[i][j-1].representation == "| O " && plateau[i][j+1].representation == "| O "){ // Test Vertical
+                                    return true;
+                                }else{
+                                    return false;
+                                }
+                            }
+                            if( i > 0 && j > 0 && i < size -1 && j < size-1){
+                                if (plateau[i-1][j-1].representation == "| O " && plateau[i+1][j+1].representation == "| O "){ // Test diagonale
+                                    return true;
+                                }else{
+                                    return false;
+                                }
+                            }
+                        }
+                    }
+                }
             }
         }
-        return;
+        return false;
+    }
+
+    public void deroulementPartie(){
+        boolean partieFinie = isOver(gameCounter, drawCounter);
+        boolean partieGagnee = isWinning();
+
+        while(!partieFinie && !partieGagnee){
+            tourJoueur1();
+            partieGagnee = isWinning();
+            partieFinie = isOver(gameCounter, drawCounter);
+
+            if(partieGagnee){
+                System.out.println("Bien joué vous avez gagné!");
+                return;
+            }
+            if(partieFinie){
+                System.out.println("Match Nul!!!");
+                return;
+            }
+            tourJoueur2();
+            partieGagnee = isWinning();
+            partieFinie = isOver(gameCounter, drawCounter);
+            if(partieGagnee){
+                System.out.println("Bien joué vous avez gagné!");
+                return;
+            }
+            if(partieFinie){
+                System.out.println("Match Nul!!!");
+                return;
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+//        while(!partieFinie && !partieGagnee){
+//
+//            tourJoueur1();
+//            partieGagnee = isWinning();
+//            partieFinie = isOver(gameCounter, drawCounter);
+//
+//            if(!partieGagnee){
+//                if (!partieFinie){
+//                    tourJoueur2();
+//                    partieGagnee=isWinning();
+//                    partieFinie=isOver(gameCounter, drawCounter);
+//                }else{
+//                    break;
+//                }
+//            }else {
+//                System.out.println("Gagné Bien Joué!");
+//            }
+//            break;
+//        }
+
+//        while(!partieFinie && !partieGagnee){
+//
+//            tourJoueur1();
+//            partieGagnee = isWinning();
+//            partieFinie = isOver(gameCounter, drawCounter);
+//
+//            if(!partieGagnee){
+//                if (!partieFinie){
+//
+//                    tourJoueur2();
+//                    partieGagnee = isWinning();
+//                }else{
+//                    partieFinie = isOver(gameCounter, drawCounter);
+//                    break;
+//                }
+//            }else{
+//                System.out.println("Gagné! bien joué!");
+//            }
+//
+//
+////            if (!partieFinie){
+////
+////                tourJoueur2();
+////                partieGagnee = isWinning();
+////                partieFinie = isOver(gameCounter, drawCounter);
+////
+////            }else{
+////                break;
+////            }
+////        }
+//        if(partieGagnee){
+//            System.out.println("Gagné! Bien joué!");
+//        }else
+//        if(partieFinie){
+//            System.out.println("Match Nul ! ");
+//        }
+//        return;
     }
 
 }
